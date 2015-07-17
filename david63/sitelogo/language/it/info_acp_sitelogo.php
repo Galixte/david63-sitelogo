837c51329d31955ea9e80b56dd66ba27188f94c8
Mauron <Mauron@users.noreply.github.com>
2015-07-17 18:55:32 +0200

    Update for Italian translation.
    Here is a refinement for actual Italian translation.
    Verbs built over English ones have been removed, text has been naturalised and UTF-8 ’ character has been used instead of \' combination.

--- david63/sitelogo/language/it/info_acp_sitelogo.php
+++ david63/sitelogo/language/it/info_acp_sitelogo.php
@@ -39,37 +39,37 @@ $lang = array_merge($lang, array(
 
 	'SITE_LOGO'						=> 'Logo personalizzato',
 
-	'SITE_LOGO_EXPLAIN'				=> 'Impostare le opzioni per un logo personalizzato che sostituisca quello di default.',
+	'SITE_LOGO_EXPLAIN'				=> 'Da questa pagina è possibile modificare le impostazioni per mostrare un logo personalizzato che sostituisca quello di default.',
 
 	'SITE_LOGO_HEIGHT'				=> 'Altezza logo',
-	'SITE_LOGO_HEIGHT_EXPLAIN'		=> 'Lasciare vuoto questo campo per utilizzare l\'altezza originale del logo.<br />L\'altezza predefinita è 52px.',
+	'SITE_LOGO_HEIGHT_EXPLAIN'		=> 'Lasciare vuoto questo campo per usare l’altezza originale del logo.<br />L’altezza predefinita è 52px.',
 
-	'SITE_LOGO_IMAGE'				=> 'Path del logo personalizzato',
-	'SITE_LOGO_IMAGE_EXPLAIN'		=> 'Lasciando vuoto questo campo, verrà utilizzato il logo predefinito.<br />Se si desidera utilizzare un\'immagine remota come logo, inserire l\'URL completo dell\'immagine, altrimenti inserire la posizione dell\'immagine sul sito.',
+	'SITE_LOGO_IMAGE'				=> 'Percorso logo personalizzato',
+	'SITE_LOGO_IMAGE_EXPLAIN'		=> 'Lasciare vuoto questo campo per usare il logo predefinito.<br />Se si desidera utilizzare un’immagine remota come logo, inserire l’URL completo dell’immagine, altrimenti inserire la posizione dell’immagine sul sito.',
 
-	'SITE_LOGO_LEFT'				=> 'Angoli del lato sinistro',
+	'SITE_LOGO_LEFT'				=> 'Angoli a sinistra',
 	'SITE_LOGO_LEFT_EXPLAIN'		=> 'Arrotondare gli angoli del lato sinistro.',
-	'SITE_LOGO_LOG'					=> '<strong>Opzioni logo personalizzato aggiornate</strong>',
+	'SITE_LOGO_LOG'					=> '<strong>Impostazioni logo personalizzato aggiornate</strong>',
 
 	'SITE_LOGO_MANAGE'				=> 'Impostazioni',
 
 	'SITE_LOGO_OPTIONS'				=> 'Opzioni logo',
 
-	'SITE_LOGO_PIXELS'				=> 'Numero di pixel',
-	'SITE_LOGO_PIXELS_EXPLAIN'		=> 'Settare il numero di pixel per l\'arrotondamento degli angoli.',
-	'SITE_LOGO_POSITION'			=> 'Posizione del logo',
+	'SITE_LOGO_PIXELS'				=> 'Numero pixel',
+	'SITE_LOGO_PIXELS_EXPLAIN'		=> 'Specificare il numero di pixel per l’arrotondamento degli angoli.',
+	'SITE_LOGO_POSITION'			=> 'Posizione logo',
 
-	'SITE_LOGO_REMOVE'				=> 'Rimuovere il logo del sito',
-	'SITE_LOGO_REMOVE_EXPLAIN'		=> 'Settando questa opzione, verrà nascosto il logo del sito.',
-	'SITE_LOGO_RIGHT'				=> 'Angoli del lato destro',
+	'SITE_LOGO_REMOVE'				=> 'Rimuovere logo',
+	'SITE_LOGO_REMOVE_EXPLAIN'		=> 'Impostando quest’opzione, verrà nascosto il logo del sito.',
+	'SITE_LOGO_RIGHT'				=> 'Angoli a destra',
 	'SITE_LOGO_RIGHT_EXPLAIN'		=> 'Arrotondare gli angoli del lato destro.',
 
 	'SITE_LOGO_WIDTH'				=> 'Larghezza logo',
 	'SITE_LOGO_WIDTH_EXPLAIN'		=> 'Lasciare vuoto questo campo per utilizzare la larghezza originale del logo.<br />La larghezza predefinita è 149px.',
 
-	'SITE_NAME_SUPRESS'				=> 'Rimuovere la visualizzazione del testo',
-	'SITE_NAME_SUPRESS_EXPLAIN'		=> 'Settando questa opzione, verrà nascosto il <strong>Nome</strong> e la <strong>Descrizione</strong> del sito.',
+	'SITE_NAME_SUPRESS'				=> 'Rimuovere visualizzazione del testo',
+	'SITE_NAME_SUPRESS_EXPLAIN'		=> 'Impostando quest’opzione, verranno nascosti <strong>nome</strong> e <strong>descrizione</strong> del sito.',
 
-	'SITE_SEARCH_REMOVE'			=> 'Rimuovere il box "Cerca..."',
-	'SITE_SEARCH_REMOVE_EXPLAIN'	=> 'Settando questa opzione, verrà rimosso il box di ricerca dall\'header.',
+	'SITE_SEARCH_REMOVE'			=> 'Rimuovere box ricerca',
+	'SITE_SEARCH_REMOVE_EXPLAIN'	=> 'Impostando quest’opzione, verrà rimosso il box di ricerca dall’header.',
 ));
